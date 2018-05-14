dd8044119d23ad2f9c518616cdf27527a09a2cf7
Riya Shah <33380157+riyashah17@users.noreply.github.com>
2018-05-14 13:32:34 +0000

    Create Magic numbers.cpp
    Given two function, one is h(x) which includes the products of all the number in an array A[ ] having size N and another function f(x) which denotes the GCD of all the numbers in an array.Your task is to find the value of  h(x)^f(x).

---
+++ Magic numbers.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long int gcd (long long int a ,long long int b){
+    if(a==0)
+        return b;
+    return gcd(b%a,a);
+}
+
+long long int calgcd(long long int arr[], int n){
+    long long int result=arr[0];
+    for(int i=1;i<n;i++){
+        result = gcd(result,arr[i]);
+    }
+    return result;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long int a[65];
+
+int main() {
+	long long int t,n,i;
+	cin>>t;
+	while(t--){
+	    int MOD=1e9+7;
+	    cin>>n;
+	    for(i=0;i<n;i++)
+	        cin>>a[i];
+	    long long int p=1,g;
+	    for(i=0;i<n;i++)
+	        p=((p*a[i])%MOD);
+	    g = calgcd(a,n);
+	   // cout<<p<<g;
+	    long long int k= pow(p,g);
+	    k=k%MOD;
+	    
+	    cout<<k<<"\n";
+	    
+	}
+	return 0;
+}
