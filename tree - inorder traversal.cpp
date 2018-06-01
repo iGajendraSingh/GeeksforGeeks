166d111aa593447ce064fb5190878159a1b78a1c
Riya Shah <33380157+riyashah17@users.noreply.github.com>
2018-06-01 14:53:07 +0000

    Create tree - inorder traversal.cpp

---
+++ tree - inorder traversal.cpp
@@ -0,0 +1,92 @@
+{
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+/* Helper function that allocates a new node with the
+   given data and NULL left and right pointers. */
+struct Node* newNode(int data)
+{
+  struct Node* node = new Node;
+  node->data = data;
+  node->left = NULL;
+  node->right = NULL;
+  return(node);
+}
+/* Computes the number of nodes in a tree. */
+void inOrder(struct Node* root);
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  scanf("%d
+", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d
+",&n);
+     Node *root = NULL;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+      //  cout << n1 << " " << n2 << " " << (char)lr << endl;
+        if (m.find(n1) == m.end())
+        {
+           parent = newNode(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+        Node *child = newNode(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+     inOrder(root);
+     cout << endl;
+  }
+  return 0;
+}
+
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child  
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+/* Prints inorder traversal of Binary Tree.  In output
+   all keys should be separated by space. For example
+  inorder traversal of below tree should be "20 10 30"
+         10
+       /      \ 
+   20       30 */
+void inOrder(Node* root)
+{
+  // Your code here
+  if(root==NULL)
+    return;
+  inOrder(root->left);
+  cout<<root->data<<" ";
+  inOrder(root->right);
+}
