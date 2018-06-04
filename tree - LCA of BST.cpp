cc95373801ab9dd5aefcf59a6a2dfa76e610a2d9
Riya Shah <33380157+riyashah17@users.noreply.github.com>
2018-06-04 15:24:49 +0530

    Create tree - LCA of BST.cpp

---
+++ tree - LCA of BST.cpp
@@ -0,0 +1,78 @@
+{
+//Code by 1shubhamjoshi1
+#include<bits/stdc++.h>
+using namespace std;
+struct Node {
+int data;
+struct Node * right, * left;
+};
+void insert(Node ** tree, int val)
+{
+    Node *temp = NULL;
+    if(!(*tree))
+    {
+        temp = (Node *)malloc(sizeof(Node));
+        temp->left = temp->right = NULL;
+        temp->data = val;
+        *tree = temp;
+        return;
+    }
+    if(val < (*tree)->data)
+    {
+        insert(&(*tree)->left, val);
+    }
+    else if(val > (*tree)->data)
+    {
+        insert(&(*tree)->right, val);
+    }
+}
+Node* LCA(Node *root, int , int );
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Node *root;
+        Node *tmp;
+    //int i;
+        root = NULL;
+        int N;
+        cin>>N;
+        for(int i=0;i<N;i++)
+        {
+            int k;
+            cin>>k;
+            insert(&root, k);
+        }
+        int l,r;
+        cin>>l>>r;
+        cout<<LCA(root,l,r)->data<<endl;
+    }
+}
+
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/*The structure of a BST Node is as follows:
+struct Node {
+    int data;
+    Node * right, * left;
+};*/
+/*You are required to complete
+this function*/
+Node* LCA(Node *root, int n1, int n2)
+{
+   //Your code here
+    // if(root==NULL)
+    //     return;
+    if(root->data > max(n1,n2))
+        return(LCA(root->left,n1,n2));
+    else if(root->data < min(n1,n2))
+        return(LCA(root->right,n1,n2));
+    else
+        return root;
+}
